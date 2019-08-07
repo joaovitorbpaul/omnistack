97caa98d0b670ccd2da4d291b36ce2e0baf732e8
João Vitor Paul <joao.paul@neogrid.com>
2019-08-07 20:13:22 -0300

    backend tindev - omnistack 8.0

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,21 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggerDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev not exists" });
+    }
+
+    loggerDev.dislikes.push(targetDev._id);
+
+    await loggerDev.save();
+
+    return res.json(loggerDev);
+  }
+};
